b492e0f76f3d0068cf68321f5e2a2367810e6ac4
rao_s <sanjan.rao@xoriant.com>
2019-04-22 15:37:43 +0530

    Add unit tests for calculator

--- test_calculator.py
+++ test_calculator.py
@@ -16,4 +16,5 @@ class TestCalculator:
     def test_multiplication(self):
         assert 100 == calculate.multiply(10, 10)
 
-
+    def test_division(self):
+        assert 1 == calculate.division(10, 10)
